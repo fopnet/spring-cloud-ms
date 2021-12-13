4e2cef8b3b2942b2cd3b474a2bfd7916b85e7df3
Felipe Pina <fop.net@gmail.com>
2021-12-13 20:54:44 -0300

    user-ms worker-ms ms projects

---
+++ worker-ms/src/main/java/com/workerms/WorkerMsApplication.java
@@ -0,0 +1,13 @@
+package com.workerms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkerMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkerMsApplication.class, args);
+	}
+
+}
---
+++ worker-ms/src/test/java/com/workerms/WorkerMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workerms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkerMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
