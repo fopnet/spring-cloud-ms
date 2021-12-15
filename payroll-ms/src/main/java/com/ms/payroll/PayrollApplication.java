6bee598d4fd90e6ad0816e0fbc81b6a36d11082c
Felipe Pina <fop.net@gmail.com>
2021-12-15 20:23:40 -0300

    ribbon load balancing

--- payroll-ms/src/main/java/com/ms/payroll/PayrollApplication.java
+++ payroll-ms/src/main/java/com/ms/payroll/PayrollApplication.java
@@ -2,10 +2,12 @@ package com.ms.payroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@RibbonClient(name = "ms-worker")
 public class PayrollApplication {
 
 	public static void main(String[] args) {
--- payroll-ms/src/main/java/com/ms/payroll/feignClient/WorkerFeignClient.java
+++ payroll-ms/src/main/java/com/ms/payroll/feignClient/WorkerFeignClient.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Component
-@FeignClient(name = "ms-worker", url = "localhost:8001", path = "/workers")
+@FeignClient(name = "ms-worker", path = "/workers")
 public interface WorkerFeignClient {
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id);
