11aaa74a3a57326d09b1c1be2bb5b5907d1d98c9
Felipe Pina <fop.net@gmail.com>
2021-12-22 13:23:53 -0300

    api-gateway-zuu created

---
+++ api-gateway-zuul-ws/src/main/java/com/ms/apigatewayzuul/ApiGatewayZuulWsApplication.java
@@ -0,0 +1,13 @@
+package com.ms.apigatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayZuulWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayZuulWsApplication.class, args);
+	}
+
+}
