ee98ba06afc128b41be59ecb2de06fd5d667a9a5
Felipe Pina <fop.net@gmail.com>
2021-12-22 08:58:47 -0300

    configuring eureka client

--- payroll-ms/src/main/java/com/ms/payroll/PayrollApplication.java
+++ payroll-ms/src/main/java/com/ms/payroll/PayrollApplication.java
@@ -2,12 +2,13 @@ package com.ms.payroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-@RibbonClient(name = "ms-worker")
+@EnableEurekaClient
+// @RibbonClient(name = "ms-worker") eureka already existe load balance
 public class PayrollApplication {
 
 	public static void main(String[] args) {
