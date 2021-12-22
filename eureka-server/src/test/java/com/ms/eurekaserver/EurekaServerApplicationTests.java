77ba201a7e1ec59e84b1b740c806534ae6b5479b
Felipe Pina <fop.net@gmail.com>
2021-12-22 08:08:07 -0300

    eureka-project

---
+++ eureka-server/src/test/java/com/ms/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
