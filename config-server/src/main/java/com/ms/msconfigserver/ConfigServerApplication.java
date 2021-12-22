07ec84f4832687bd4e490c037ab5a08680634c53
Felipe Pina <fop.net@gmail.com>
2021-12-22 16:11:21 -0300

    private git repository wirh personal temporary token

--- config-server/src/main/java/com/ms/msconfigserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/ms/msconfigserver/ConfigServerApplication.java
@@ -1,5 +1,7 @@
 package com.ms.msconfigserver;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
@@ -8,10 +10,18 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableConfigServer
-public class ConfigServerApplication {
+public class ConfigServerApplication implements CommandLineRunner {
 
+	@Value("${spring.cloud.config.server.git.username}")
+	private String gitUserName;
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("GITHUB_USER:" + gitUserName);
+
 	}
 
 }
