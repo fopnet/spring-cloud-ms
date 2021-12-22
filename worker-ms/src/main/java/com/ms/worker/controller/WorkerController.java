02b113044681020db995f63bb30010d8a652f0e8
Felipe Pina <fop.net@gmail.com>
2021-12-22 15:31:59 -0300

    actuator for runtime config refresh

--- worker-ms/src/main/java/com/ms/worker/controller/WorkerController.java
+++ worker-ms/src/main/java/com/ms/worker/controller/WorkerController.java
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/workers")
+@RefreshScope // actuator config
 public class WorkerController {
 
     @Autowired
