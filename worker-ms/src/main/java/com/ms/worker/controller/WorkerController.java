f59418b70b01fbdc5e43a66fa9c793807d319a70
Felipe Pina <fop.net@gmail.com>
2021-12-22 08:11:18 -0300

    eureka-project .mvn

--- worker-ms/src/main/java/com/ms/worker/controller/WorkerController.java
+++ worker-ms/src/main/java/com/ms/worker/controller/WorkerController.java
@@ -5,7 +5,10 @@ import java.util.List;
 import com.ms.worker.entity.Worker;
 import com.ms.worker.repository.WorkerRepository;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerController {
+
+    @Autowired
+    private Environment environment;
+
+    private static Logger logger = LoggerFactory.getLogger(WorkerController.class);
 
     @Autowired
     private WorkerRepository repository;
@@ -28,6 +36,8 @@ public class WorkerController {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
+        logger.info("PORT " + environment.getProperty("local.server.port"));
+
         Worker wk = repository.findById(id).orElseThrow(() -> new RuntimeException("Worker not found"));
         return ResponseEntity.ok(wk);
     }
